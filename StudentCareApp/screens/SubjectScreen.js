a29ec6dd36dae2a9e26fc561f7db9337cc0bc929
Apsara2001 <apsaraduanji2001@gmail.com>
2025-01-05 22:44:29 +0530

    Adding styles to SubjectScreen.js

--- StudentCareApp/screens/SubjectScreen.js
+++ StudentCareApp/screens/SubjectScreen.js
@@ -69,4 +69,98 @@ const SubjectScreen = ({ route }) => {
     </>
   );
 };
+const styles = StyleSheet.create({
+    bannerContainer: {
+      alignItems: "center",
+    },
+    image: {
+      width: 350,
+      borderRadius: 10,
+      marginTop: -25,
+    },
+    courseCard: {
+      alignItems: "center",
+      backgroundColor: "#fff",
+      marginHorizontal: 30,
+      paddingHorizontal: 15,
+      paddingTop: 25,
+      paddingBottom: 45,
+      elevation: 5,
+      marginTop: -25,
+    },
+    profileImage: {
+      width: 200,
+      height: 200,
+    },
+    name: {
+      fontSize: 34,
+      fontWeight: "bold",
+      marginTop: 10,
+      textAlign: "center",
+    },
+    subDetails: {
+      fontSize: 17,
+      marginTop: 10,
+      textAlign: "center",
+    },
+    divider: {
+      width: 300,
+      height: 2,
+      marginTop: 30,
+    },
+    marksInfoHeader: {
+      width: "90%",
+      fontSize: 17,
+      marginTop: 20,
+      fontWeight: "bold",
+    },
+    markDetailsContainer: {
+      width: "90%",
+      marginTop: 15,
+      gap: 2,
+    },
+    markDetails: {
+      fontSize: 14,
+      lineHeight: 21,
+    },
+    footer: {
+      backgroundColor: "#70116d",
+      marginHorizontal: 15,
+      position: "absolute",
+      bottom: 0,
+      paddingVertical: 8,
+      marginBottom: 10,
+    },
+    footerText: {
+      fontSize: 15,
+      textAlign: "center",
+      color: "#fff",
+    },
+    tableHeader: {
+      width: "90%",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingHorizontal: 15,
+      gap: 150,
+    },
+    secondDivider: {
+      width: 300,
+      height: 2,
+      marginTop: 10,
+      opacity: 0.35,
+    },
+    tableData: {
+      width: "90%",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      paddingHorizontal: 15,
+    },
+    tdText: {
+      fontWeight: "bold",
+      fontSize: 17,
+      marginTop: 10,
+    },
+  });
 export default SubjectScreen;
