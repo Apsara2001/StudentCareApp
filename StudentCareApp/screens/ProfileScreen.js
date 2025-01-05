ff236791e9a37e285ceee4620780c3cea1e576f8
Apsara2001 <apsaraduanji2001@gmail.com>
2025-01-05 22:09:01 +0530

    Adding styles to ProfileScreen.js

--- StudentCareApp/screens/ProfileScreen.js
+++ StudentCareApp/screens/ProfileScreen.js
@@ -60,4 +60,84 @@ const ProfileScreen = ({ route }) => {
     </>
   );
 };
+const styles = StyleSheet.create({
+    bannerContainer: {
+      alignItems: "center",
+    },
+    image: {
+      width: 350,
+      borderRadius: 10,
+      marginTop: -25,
+    },
+    profileCard: {
+      alignItems: "center",
+      backgroundColor: "#fff",
+      marginHorizontal: 30,
+      paddingHorizontal: 15,
+      paddingTop: 25,
+      paddingBottom: 45,
+      elevation: 5,
+      marginTop: -25,
+    },
+    profileImage: {
+      width: 200,
+      height: 200,
+    },
+    name: {
+      fontSize: 34,
+      fontWeight: "bold",
+      marginTop: 10,
+      textAlign: "center",
+    },
+    subDetails: {
+      fontSize: 17,
+      marginTop: 10,
+      textAlign: "center",
+    },
+    divider: {
+      width: 300,
+      height: 2,
+      marginTop: 30,
+    },
+    contactHeader: {
+      width: "90%",
+      fontSize: 17,
+      marginTop: 20,
+      fontWeight: "bold",
+    },
+    contactDetailsContainer: {
+      width: "90%",
+      marginTop: 15,
+      gap: 2,
+    },
+    contactDetails: {
+      fontSize: 14,
+    },
+    bilogicalHeader: {
+      width: "90%",
+      fontSize: 17,
+      marginTop: 20,
+      fontWeight: "bold",
+    },
+    bilogicalDetailsContainer: {
+      width: "90%",
+      marginTop: 15,
+      gap: 2,
+    },
+    bilogicalDetails: {
+      fontSize: 14,
+    },
+    footer: {
+      backgroundColor: "#70116d",
+      marginHorizontal: 15,
+      marginTop: 15,
+      paddingVertical: 8,
+      marginBottom: 10,
+    },
+    footerText: {
+      fontSize: 15,
+      textAlign: "center",
+      color: "#fff",
+    },
+  });
 export default ProfileScreen;
